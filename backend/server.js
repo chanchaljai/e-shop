0581da7c8624c7342d723506838d8a03db7eba69
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-13 16:30:55 +0530

    add to cart

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
-app.use("/cart", cartRoute);
+app.use("/api/cart", cartRoute);
 
 app.get("/", (req, res) => {
     res.send("API is running...");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,44 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import ProductDetails  from "./pages/ProductDetails";
+import ProductDetails from "./pages/ProductDetails";
 import AddProduct from "./admin/AddProduct";
 import EditProduct from "./admin/EditProduct";
 import ProductList from "./admin/ProductList";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import CheckoutAddress from "./pages/CheckoutAddress";
+import Checkout from "./pages/Checkout";
 
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
-  {path: "/", element: <Home />},
-  {path: "/login", element: <Login />}, 
-  {path: "/signup", element: <Signup />},
-  {path: "/product/:id", element: <ProductDetails />},
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/product/:id", element: <ProductDetails /> },
+      { path : "/cart", element: <Cart />},
 
-  {path: "/admin/products", element: <ProductList />},
-  {path: "/admin/products/add", element: <AddProduct />},
-  {path: "/admin/products/edit/:id", element: <EditProduct />}
-  ]);
+      { path: "/admin/products", element: <ProductList /> },
+      { path: "/admin/products/add", element: <AddProduct /> },
+      { path: "/admin/products/edit/:id", element: <EditProduct /> },
+      { path: "/checkout-address", element: <CheckoutAddress /> },
+      { path: "/checkout", element: <Checkout /> },
+    ],
+  },
+]);
 
 export default function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
